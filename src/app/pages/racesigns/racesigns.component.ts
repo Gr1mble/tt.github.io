b959f34ecdb12a7792a535b04c32c926d29e02b5
Gr1mble <Jkirkwood5409@gmail.com>
2023-07-22 17:08:45 -0400

    RaceSigns Fix

--- src/app/pages/racesigns/racesigns.component.ts
+++ src/app/pages/racesigns/racesigns.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-racesigns',
   templateUrl: './racesigns.component.html',
-  styleUrls: ['./racesigns.component.css']
+  styleUrls: ['./racesigns.component.css', './_racesigns.scss']
 })
 export class RacesignsComponent {
 
