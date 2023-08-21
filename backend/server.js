e055d88901b6e3674af2b1b8ceebb306af7f68eb
Gr1mble <Jkirkwood5409@gmail.com>
2023-08-21 11:51:31 -0400

    push

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
 const http = require('http');
+/*const https = require('https');*/
+
 const express = require('./rest.js');
 
-const server = http.createServer(express);
+/* const sslCert = "C:/Users/Administrator/Documents/Certs/cert.pem";
+const sslKey = "C:/Users/Administrator/Documents/Certs/privkey.pem";
 
-server.listen(3000, function check(err){
+const httpsServer = https.createServer(express, sslCert, sslKey);
+
+httpsServer.listen(3001, function check(err){
     if(err){
-        console.log("Error Starting Server on 3000")
+        console.log("Error Starting HTTPS Server on 3001")
     } else {
-        console.log("Starting Server on 3000")
+        console.log("Starting HTTPS Server on 3001")
+    }
+}) */
+
+const httpServer = http.createServer(express);
+
+httpServer.listen(3000, function check(err){
+    if(err){
+        console.log("Error Starting HTTP Server on 3000")
+    } else {
+        console.log("Starting HTTP Server on 3000")
     }
 })
