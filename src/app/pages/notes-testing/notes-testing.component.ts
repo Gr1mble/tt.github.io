6eb02a3a1940ebe6af5fa30c71fc5c576d269eec
Gr1mble <Jkirkwood5409@gmail.com>
2023-09-05 15:39:14 -0400

    Notes-Testing function works

--- src/app/pages/notes-testing/notes-testing.component.ts
+++ src/app/pages/notes-testing/notes-testing.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-notes-testing',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./notes-testing.component.css']
 })
 export class NotesTestingComponent {
+  @ViewChild('notesBox') notes!: ElementRef;
+
+
+  yearInput: number = 0;
+
+  notesYear(yearInput: number): void {
+    this.yearInput = yearInput;
+    console.log(this.yearInput);
+
+    this.notes.nativeElement.innerHTML = this.notesArray[this.yearInput];
+  }
+
+  notesArray = [
+    ["Test for year 23"],
+    ["Test for year 22"],
+    ["Test for year 21"],
+    ["Test for year 20"],
+    ["Test for year 19"],
+    ["Test for year 18"]
+  ]
 
 }
