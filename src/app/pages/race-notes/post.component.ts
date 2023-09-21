d4bca725cb8ef2b84e6f74fb22ac16f109f25f03
Gr1mble <Jkirkwood5409@gmail.com>
2023-09-21 18:47:15 -0400

    Works with refresh

--- src/app/pages/race-notes/post.component.ts
+++ src/app/pages/race-notes/post.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, OnDestroy, ElementRef, ViewChild} from '@angular/core';
 import { AuthService } from '../../../../backend/auth-service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { PostEntry } from 'backend/postEntry.model';
 import { PostDataService } from 'backend/postData.component';
-import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-race-notes',
@@ -15,16 +15,23 @@ import { MatDialog } from '@angular/material/dialog';
 export class PostComponent implements OnInit, OnDestroy {
   @ViewChild('notesBox') notes!: ElementRef;
   @ViewChild('notesHeader') header!: ElementRef;
-  @ViewChild('editModal') edit!: ElementRef;
 
-  showModal = false;
-  modalTitle = '';
-  modalContent = '';
+  editMode = false;
+  private paramId!: string;
+
+  postEntry!: PostEntry;
+
+  postForm!: FormGroup;
 
   private authenticationSub!: Subscription;
   isAuthenticated = false;
+  isSelected = false;
 
-  constructor(private PostDataService: PostDataService, private router: Router, private authService: AuthService,private dialogRef : MatDialog) {}
+  constructor(
+    private PostDataService: PostDataService, 
+    private router: Router, 
+    private authService: AuthService,
+    ) {}
   
   ngOnDestroy(): void {
     this.PostEntriesSub.unsubscribe();
@@ -34,9 +41,13 @@ export class PostComponent implements OnInit, OnDestroy {
   PostEntries: PostEntry[] = [];
   PostEntriesSub = new Subscription();
   currentPostId = "";
-  display = 'none';
 
   ngOnInit(): void {
+    this.postForm = new FormGroup({
+      'year': new FormControl(this.editMode ? this.postEntry.year : '', [Validators.required]),
+      'entry': new FormControl(this.editMode ? this.postEntry.entry : '', [Validators.required])
+    })
+    
     this.PostDataService.getPostEntries();
     this.PostEntriesSub = this.PostDataService.PostSubject.subscribe(entries => {
       this.PostEntries = entries;
@@ -45,31 +56,52 @@ export class PostComponent implements OnInit, OnDestroy {
       this.isAuthenticated = status;
     })
     this.isAuthenticated = this.authService.getIsAuthenticated();
-  }
+}
 
   onDelete(){
     this.PostDataService.onDeleteEntry(this.currentPostId);
   }
 
   onEdit(){
-    this.router.navigate(['edit',this.currentPostId])
-  }
+    this.postEntry = this.PostDataService.getPostEntry(this.currentPostId);
+    this.editMode = true;
 
-  onNewPost(){
-    this.router.navigate(['data-entry'])
+    this.postForm.setValue({
+      year: this.PostDataService.getPostEntry(this.currentPostId).year, 
+      entry: this.PostDataService.getPostEntry(this.currentPostId).entry
+    });
   }
 
   getEntryPost(id: string){
+    this.currentPostId = id;
     this.notes.nativeElement.innerHTML = this.PostDataService.getPostEntry(id).entry;
     this.header.nativeElement.innerHTML = this.PostDataService.getPostEntry(id).year;
-    this.currentPostId = id;
+
+    if(this.currentPostId != ''){
+      this.isSelected = true;
+  } else {
+      this.isSelected = false;
+  }
   }
 
-  openDialog(){
-    this.dialogRef.open(PostComponent,{
-      data : {
-        name : 'Samuel'
+    onSubmit() {
+    const entry = new PostEntry('', this.postForm.value.year, this.postForm.value.entry);
+  
+    if (this.editMode) {
+      entry.id = this.currentPostId;
+
+      try {
+        // Update the entry and wait for it to complete
+        this.PostDataService.updateEntry(this.currentPostId, entry);
+
+      } catch (error) {
+        console.error('Error updating entry:', error);
       }
-    });
+    } else {
+      this.PostDataService.onAddPostEntry(entry);
+    }
+   
   }
+  
+  
 }
